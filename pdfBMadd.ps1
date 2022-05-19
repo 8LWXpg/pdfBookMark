10159e02cb92d364235c6cb522e9e1643cb16f2d
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2022-05-19 16:08:36 +0800

    Add files via upload

---
+++ pdfBMadd.ps1
@@ -0,0 +1,42 @@
+param (
+    [Parameter(ValueFromPipeline = $true)]
+    [System.IO.FileInfo]$pdf,
+    [string]$str
+)
+
+if (-not $pdf) {
+    Read-Host 'pdf file path'
+}
+if (-not $str) {
+    Read-Host 'string to find'
+}
+
+$get = gswin64c -sDEVICE=txtwrite -o- $pdf | Select-String $str, 'page'
+if (!$?) {
+    return
+}
+
+$out = @()
+$count = 0
+switch -regex ($get) {
+    'page (\d+)' { $page = $Matches[1] }
+    "$str +(\d+)" {
+        if ($Matches[1] -eq $count + 1) {
+            $out += $page
+            $count = $Matches[1].ToInt32($null)
+        }
+    }
+    Default {}
+}
+
+$temp = "$($pdf.DirectoryName)\$($pdf.BaseName)_BMadd$($pdf.Extension)"
+pdftk $pdf dump_data_utf8 output - | ForEach-Object {
+    if ($_ -match 'NumberOfPages') {
+        for ($i = 0; $i -lt $out.Count; $i++) {
+            'BookmarkBegin'
+            "BookmarkTitle: $str $($i+1)"
+            "BookmarkLevel: 1"
+            "BookmarkPageNumber: $($out[$i])"
+        }
+    }
+} | pdftk $pdf update_info_utf8 - output $temp
